05477b425d5be5e387475a397f8c35b2fddd0504
Nico Busse <156131173+nico-busse@users.noreply.github.com>
2025-05-15 16:45:07 -0400

    add storage backend

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Amplify } from 'aws-amplify';
+import awsExports from '../amplify/aws-exports';
+
+Amplify.configure(awsExports);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
